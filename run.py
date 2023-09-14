38cd75dae050086d228370f2ae801e519c311432
Masorr <>
2023-09-14 16:44:28 +0000

    Add choose difficulty function + add new game function caller

--- run.py
+++ run.py
@@ -1,37 +1,54 @@
-while True:
-    difficulty = input(f"Insert difficulty (easy/normal/hard): \n")
+def choose_difficulty():
+    """
+    Select between three difficulties:
+    easy: 5 by 5 grid
+    normal: 7 by 7 grid
+    hard: 10 by 10 grid
+    """
 
-    if difficulty == "easy":
-        grid_size = 5
-        break
-    elif difficulty == "normal":
-        grid_size = 7
-        break
-    elif difficulty == "hard":
-        grid_size = 10
-        break
-    else:
-        print("Invalid difficulty choice")
+    while True:
+        difficulty = input(f"Insert difficulty (easy/normal/hard): \n")
 
-rules_variable = f"""
-{'-'*80}
-#    Welcome to BATTLESHIPS the game!
-#    Difficulty: {difficulty}
-#    Map size: {grid_size} by {grid_size} grid
-#
-#    Rules:
-#    Sink the computer's 5 ships before it sinks yours!
-#    You and the computer have each a separate grid with 5 ships
-#    Select a row number (0 to {grid_size - 1}), then a column number (0 to {grid_size - 1})
-#    Used coordinates are removed from game!
-#    Good luck!
-{'-'*80}
-"""
+        if difficulty == "easy":
+            grid_size = 5
+            break
+        elif difficulty == "normal":
+            grid_size = 7
+            break
+        elif difficulty == "hard":
+            grid_size = 10
+            break
+        else:
+            print("Invalid difficulty choice")
 
-def rules():
+    return difficulty, grid_size
+
+def rules(difficulty, grid_size):
+    """
+    Prints the rules for the chosen difficulty of the game.
+    """
+
+    rules_variable = f"""
+    {'-'*80}
+    #    Welcome to BATTLESHIPS the game!
+    #    Difficulty: {difficulty}
+    #    Map size: {grid_size} by {grid_size} grid
+    #
+    #    Rules:
+    #    Sink the computer's 5 ships before it sinks yours!
+    #    You and the computer have each a separate grid with 5 ships
+    #    Select a row number (0 to {grid_size - 1}), then a column number (0 to {grid_size - 1})
+    #    Used coordinates are removed from game!
+    #    Good luck!
+    {'-'*80}
+    """
     print(rules_variable)
 
-rules()
+def new_game():
+    difficulty, grid_size = choose_difficulty()
+    rules(difficulty, grid_size)
+
+new_game()
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
