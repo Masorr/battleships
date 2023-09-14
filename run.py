3a640decc066fa8607672060509f892c48f1570f
Masorr <>
2023-09-14 15:31:21 +0000

    Add input difficulty while loop

--- run.py
+++ run.py
@@ -1,16 +1,30 @@
+while True:
+    difficulty = input(f"Insert difficulty (easy/normal/hard): \n")
+
+    if difficulty == "easy":
+        grid_size = 5
+        break
+    elif difficulty == "normal":
+        grid_size = 7
+        break
+    elif difficulty == "hard":
+        grid_size = 10
+        break
+    else:
+        print("Invalid difficulty choice")
+
 rules_variable = f"""
 {'-'*80}
 #    Welcome to BATTLESHIPS the game!
-#    Difficulty: easy
-#    Map size: 5 by 5 grid
+#    Difficulty: {difficulty}
+#    Map size: {grid_size} by {grid_size} grid
 #
 #    Rules:
 #    Sink the computer's 5 ships before it sinks yours!
 #    You and the computer have each a separate grid with 5 ships
-#    Select a row number (0 to 4), then a column number (0 to 4)
+#    Select a row number (0 to {grid_size - 1}), then a column number (0 to {grid_size - 1})
 #    Used coordinates are removed from game!
 #    Good luck!
-
 {'-'*80}
 """
 
